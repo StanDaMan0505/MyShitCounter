0a6331b565ad95de461b37a26fadb449ceb4a9bd
StanDaMan0505 <daniel.readyforspam@gmail.com>
2019-11-24 21:21:50 +0100

    Projektdateien hinzufügen.

---
+++ MyShitCounter/MyShitCounter/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyShitCounter
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Button upButton;
+        Button downButton;
+        ListView list;
+        ObservableCollection<string> data = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            // InitializeComponent();
+            this.Padding = new Thickness(20, 20, 20, 20);
+            StackLayout panel = new StackLayout { Spacing = 15 };
+
+            panel.Children.Add(upButton = new Button
+            {
+                Text = "The Good"
+            });
+
+            panel.Children.Add(downButton = new Button
+            {
+                Text = "The Bad"
+            });
+
+            panel.Children.Add(list = new ListView
+            {
+                Header = "& The Ugly Truth",
+                SelectionMode = ListViewSelectionMode.None,
+                ItemsSource = data
+            });
+
+            upButton.Clicked += UpButton_Clicked;
+            downButton.Clicked += DownButton_Clicked;
+
+            this.Content = panel;
+        }
+
+        private void DownButton_Clicked(object sender, EventArgs e)
+        {
+            data.Add("down");
+        }
+
+        private void UpButton_Clicked(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            data.Add("Up");
+        }
+    }
+}
